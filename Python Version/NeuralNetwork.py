609a1cf9a91550b09b79f64a2f26df17b65d9ff3
Withanage Perera <prav.perera@gmail.com>
2022-11-08 05:32:58 -0500

    Started working with Keras API.

--- Python Version/NeuralNetwork.py
+++ Python Version/NeuralNetwork.py
@@ -1,17 +1,52 @@
 # Libraries for math and graphing
 import numpy as np
-import matplotlib.pyplot as pyplot
+# import matplotlib.pyplot as pyplot
+from random import randint
+from sklearn.utils import shuffle
+from sklearn.preprocessing import MinMaxScaler
 
-# Function for initializing dimensional parameters
-# which will ebb stored in a dictionary called params
-# W1 will represent the weight matrix for layer 1
-initialize_parameters(dimensions):
-    np.random.seed(3)
-    params = {}
-    
-    # Takes length of parameter
-    L = len(dimensions)
+train_labels = []
+train_samples = []
 
-    for l in range(1, L):
-        params['W' + str(1)] = np.random.randn(layer_dims[1], layer_dims[l-1])*0.01
+for i in range(50):
+    random_younger = randint(13, 64)
+    train_samples.append(random_younger)
+    train_labels.append(1)
 
+    random_older = randint(65, 100)
+    train_samples.append(random_older)
+    train_labels.append(0)
+
+for i in range(1000):
+    random_younger = randint(13, 64)
+    train_samples.append(random_younger)
+    train_labels.append(0)
+
+    random_older = randint(65, 100)
+    train_samples.append(random_older)
+    train_labels.append(1)
+
+train_labels = np.array(train_labels)
+train_samples = np.array(train_samples)
+train_labels, train_samples = shuffle(train_labels, train_samples)
+
+scaler = MinMaxScaler(feature_range=(0, 1))
+scaled_train_samples = scaler.fit_transform((train_samples.reshape(-1, 1)))
+
+import tensorflow as tensorflow
+from tensorflow import keras
+from tensorflow.keras.models import Sequential
+from tensorflow.keras.layers import Activation, Dense
+from tensorflow.keras.optimizers import Adam
+from tensorflow.keras.metrics import categorical_crossentropy
+
+model = Sequential(
+    [
+        Dense(units = 16, input_shape=(1,), activation = 'relu'),
+        Dense(units = 32, activation = 'relu'),
+        Dense(units = 2, activation = 'softmax')
+    ]
+)
+
+model.compile(optimizer = Adam(learning_rate=0.0001), loss = 'sparse_categorical_crossentropy', metrics = ['accuracy'])
+model.fit(x = scaled_train_samples, y=train_samples, validation_split = 0.1, batch_size = 10, epochs = 30, shuffle = True, verbose = 2)
